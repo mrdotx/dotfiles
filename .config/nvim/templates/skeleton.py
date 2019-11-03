59a00e3d31086e1d3c7eb53b3c9e8084034e3bd4
mrdotx <klassiker@hotmail.com>
2019-11-03 14:18:43 +0100

    add python template

---
+++ .config/nvim/templates/skeleton.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8:ft=python:foldmethod=marker
+
+# Path:     ~/coding/shell/<++>.py
+# User:     klassiker [mrdotx]
+# GitHub:   https://github.com/mrdotx/python
+
+<++>
