e82cde81c25a8f4a72852420643b821e69dd7089
mrdotx <klassiker@gmx.de>
2019-11-21 21:41:43 +0100

    add cmus notification

---
+++ .config/cmus/cmus_notify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# vim:fileencoding=utf-8:ft=sh:foldmethod=marker
+
+# path:       ~/coding/shell/cmus_notify.sh
+# user:       klassiker [mrdotx]
+# github:     https://github.com/mrdotx/shell
+# date:       2019-11-21 21:40:32
+
+$HOME/.config/cmus/cmus_status_display.sh "$*" & 
