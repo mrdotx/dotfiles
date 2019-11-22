5d24c15fe165ba35524bcc96812cf5d6f5d8ab38
mrdotx <klassiker@gmx.de>
2019-11-22 01:06:34 +0100

    implement polybar toggle

--- .config/polybar/cmus.sh
+++ .config/polybar/cmus.sh
@@ -4,41 +4,41 @@
 # path:       ~/.config/polybar/cmus.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dotfiles
-# date:       2019-11-21 21:58:13
+# date:       2019-11-22 01:03:10
 
 if info=$(cmus-remote -Q 2> /dev/null); then
-	status=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "status " | cut -d ' ' -f 2)
-	
-	if [ "$status" = "playing" ] || [ "$status" = "paused" ] || [ "$status" = "stopped" ]; then
-		title=$(echo "$info" | grep -v 'set ' | grep " title " | cut -d ' ' -f 3-)
-		artist=$(echo "$info" | grep -v 'set ' | grep " artist " | cut -d ' ' -f 3-)
-		position=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "position " | cut -d ' ' -f 2)
-		duration=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "duration " | cut -d ' ' -f 2)
-		
-		if [ "$duration" -ge 0 ]; then
-			pos_minutes=$(printf "%02d" $((position / 60)))
-			pos_seconds=$(printf "%02d" $((position % 60)))
+    status=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "status " | cut -d ' ' -f 2)
 
-			dur_minutes=$(printf "%02d" $((duration / 60)))
-			dur_seconds=$(printf "%02d" $((duration % 60)))
+    if [ "$status" = "playing" ] || [ "$status" = "paused" ] || [ "$status" = "stopped" ]; then
+        title=$(echo "$info" | grep -v 'set ' | grep " title " | cut -d ' ' -f 3-)
+        artist=$(echo "$info" | grep -v 'set ' | grep " artist " | cut -d ' ' -f 3-)
+        position=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "position " | cut -d ' ' -f 2)
+        duration=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "duration " | cut -d ' ' -f 2)
 
-			info_string="| $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds" 
-		fi
-		
-		info_string="$artist - $title $info_string"
-		
-		if [ "$status" = "playing" ]; then
-			echo "#1 $info_string"
-		elif [ "$status" = "paused" ]; then
-			echo "#2 $info_string"
-		elif [ "$status" = "stopped" ]; then 
-			echo "#3 $info_string"
-		else
-			echo ""
-		fi
-	else
-		echo ""
-	fi
+        if [ "$duration" -ge 0 ]; then
+            pos_minutes=$(printf "%02d" $((position / 60)))
+            pos_seconds=$(printf "%02d" $((position % 60)))
+
+            dur_minutes=$(printf "%02d" $((duration / 60)))
+            dur_seconds=$(printf "%02d" $((duration % 60)))
+
+            info_string="| $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
+        fi
+
+        info_string="$artist - $title $info_string"
+
+        if [ "$status" = "playing" ]; then
+            echo "%{F#dfdfdf}%{o#00b200} $info_string%{o-}%{F-}"
+        elif [ "$status" = "paused" ]; then
+            echo "%{F#dfdfdf}%{o#b0b200} $info_string%{o-}%{F-}"
+        elif [ "$status" = "stopped" ]; then
+            echo "%{F#dfdfdf}%{o#ff5555} $info_string%{o-}%{F-}"
+        else
+            echo ""
+        fi
+    else
+        echo ""
+    fi
 else
-	echo ""
+    echo ""
 fi
