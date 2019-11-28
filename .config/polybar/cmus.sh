bd64745183551547624d732995dfa4c8639f8214
mrdotx <klassiker@gmx.de>
2019-11-28 13:45:49 +0100

    change cmus

--- .config/polybar/cmus.sh
+++ .config/polybar/cmus.sh
@@ -4,41 +4,49 @@
 # path:       ~/.config/polybar/cmus.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/dotfiles
-# date:       2019-11-22 01:03:10
+# date:       2019-11-28 13:42:26
 
 if info=$(cmus-remote -Q 2> /dev/null); then
-    status=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "status " | cut -d ' ' -f 2)
 
-    if [ "$status" = "playing" ] || [ "$status" = "paused" ] || [ "$status" = "stopped" ]; then
-        title=$(echo "$info" | grep -v 'set ' | grep " title " | cut -d ' ' -f 3-)
-        artist=$(echo "$info" | grep -v 'set ' | grep " artist " | cut -d ' ' -f 3-)
-        position=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "position " | cut -d ' ' -f 2)
-        duration=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "duration " | cut -d ' ' -f 2)
+    status=$(echo "$info" | grep '^status ' | sed 's/^status //')
+    stream=$(echo "$info" | grep '^stream ' | sed 's/^stream //')
+    duration=$(echo "$info" | grep '^duration ' | sed 's/^duration //')
+    position=$(echo "$info" | grep '^position ' | sed 's/^position //')
+    file=$(echo "$info" | grep '^file ' | sed 's/^file //')
+    artist=$(echo "$info" | grep '^tag artist ' | sed 's/^tag artist //')
+    album=$(echo "$info" | grep '^tag album ' | sed 's/^tag album //')
+    tracknumber=$(echo "$info" | grep '^tag tracknumber ' | sed 's/^tag tracknumber //')
+    title=$(echo "$info" | grep '^tag title ' | sed 's/^tag title //')
+    genre=$(echo "$info" | grep '^tag genre ' | sed 's/^tag genre //')
+    comment=$(echo "$info" | grep '^tag comment ' | sed 's/^tag comment //')
 
-        if [ "$duration" -ge 0 ]; then
-            pos_minutes=$(printf "%02d" $((position / 60)))
-            pos_seconds=$(printf "%02d" $((position % 60)))
-
-            dur_minutes=$(printf "%02d" $((duration / 60)))
-            dur_seconds=$(printf "%02d" $((duration % 60)))
-
-            info_string="| $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
-        fi
-
-        info_string="$artist - $title $info_string"
-
-        if [ "$status" = "playing" ]; then
-            echo "%{F#dfdfdf}%{o#00b200} $info_string%{o-}%{F-}"
-        elif [ "$status" = "paused" ]; then
-            echo "%{F#dfdfdf}%{o#b0b200} $info_string%{o-}%{F-}"
-        elif [ "$status" = "stopped" ]; then
-            echo "%{F#dfdfdf}%{o#ff5555} $info_string%{o-}%{F-}"
-        else
-            echo ""
-        fi
-    else
-        echo ""
+    if [ "$duration" -ge 0 ]; then
+        positionminutes=$(printf "%02d" $((position / 60)))
+        positionseconds=$(printf "%02d" $((position % 60)))
+        durationminutes=$(printf "%02d" $((duration / 60)))
+        durationseconds=$(printf "%02d" $((duration % 60)))
+        titlestatus="$positionminutes:$positionseconds / $durationminutes:$durationseconds"
     fi
+
+    case $status in
+        "playing") info="%{F#dfdfdf}%{o#00b200} $titlestatus" ;;
+        "paused") info="%{F#dfdfdf}%{o#b0b200} $titlestatus" ;;
+        "stopped") info="%{F#dfdfdf}%{o#ff5555} $titlestatus" ;;
+        *) info="" ;;
+    esac
+
+    if [[ "$stream" == "" ]]; then
+        infobody="$artist - $title"
+    else
+        infobody="$title - $stream"
+    fi
+
+    if [[ "$artist" == "" && "$title" == "" ]]; then
+        echo "$info" "${file##*/}%{o-}%{F-}"
+    else
+        echo "$info" "$infobody%{o-}%{F-}"
+    fi
+
 else
     echo ""
 fi
