6b95a7abb137474c64ddb252bd9763d09493fce2
mrdotx <klassiker@gmx.de>
2019-12-04 16:17:13 +0100

    update help

--- .config/nvim/templates/skeleton.sh
+++ .config/nvim/templates/skeleton.sh
@@ -3,6 +3,17 @@
 # path:       ~/coding/shell/<++>.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-12-01 11:49:02
+# date:       2019-12-04 15:12:16
+
+script=$(basename "$0")
+help="$script [-h/--help] -- script to <++>
+  Usage:
+    $script [<++>]
+
+  Setting:
+    [<++>] = name of the bla to be blub
+
+  Example:
+    $script <++>"
 
 <++>
