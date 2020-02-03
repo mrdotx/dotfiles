58bf16b16ed04bea18a932d8286361f2a14682ad
mrdotx <klassiker@gmx.de>
2020-02-03 14:24:13 +0100

    initial upload

---
+++ .config/nvim/templates/skeleton.c
@@ -0,0 +1,14 @@
+/*
+ * path:       ~/projects/c/<++>.c
+ * author:     klassiker [mrdotx]
+ * github:     https://github.com/mrdotx/c
+ * date:       2020-02-03T13:58:25+0100
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    <++>
+}
