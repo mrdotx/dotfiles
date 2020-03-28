608085880c1f7984d2b5413e0f6041a474364ed7
mrdotx <klassiker@gmx.de>
2020-03-29 00:41:33 +0100

    initial upload

---
+++ .config/dwm/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# path:       ~/.config/dwm/autostart.sh
+# author:     klassiker [mrdotx]
+# github:     https://github.com/mrdotx/system
+# date:       2020-03-29T00:21:03+0100
+
+systemctl --user start window-manager.target
---
+++ .config/dwm/autostart_blocking.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# path:       ~/.config/dwm/autostart_blocking.sh
+# author:     klassiker [mrdotx]
+# github:     https://github.com/mrdotx/system
+# date:       2020-03-29T00:19:54+0100
+
