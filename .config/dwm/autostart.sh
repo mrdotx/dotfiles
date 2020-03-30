e3494f034ce5acfd75b16993402c9175021e3b9b
mrdotx <klassiker@gmx.de>
2020-03-30 09:07:32 +0200

    dwm removed

--- .config/dwm/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# path:       ~/.config/dwm/autostart.sh
-# author:     klassiker [mrdotx]
-# github:     https://github.com/mrdotx/system
-# date:       2020-03-29T00:21:03+0100
-
-systemctl --user start window-manager.target
