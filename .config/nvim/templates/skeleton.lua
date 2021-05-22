6c0a52e20c7661c2337168d406be2c0eb2bb0252
mrdotx <klassiker@gmx.de>
2021-05-22 17:48:04 +0200

    initial upload

---
+++ .config/nvim/templates/skeleton.lua
@@ -0,0 +1,6 @@
+-- path:   /home/klassiker/.local/share/repos/dotfiles/.config/nvim/templates/skeleton.lua
+-- author: klassiker [mrdotx]
+-- github: https://github.com/mrdotx/<++>
+-- date:   2021-05-22T16:35:51+0200
+
+<++>
---
+++ .config/nvim/templates/skeleton.vim
@@ -0,0 +1,6 @@
+" path:   /home/klassiker/.local/share/repos/dotfiles/.config/nvim/templates/skeleton.vim
+" author: klassiker [mrdotx]
+" github: https://github.com/mrdotx/<++>
+" date:   2021-05-22T16:34:43+0200
+
+<++>
