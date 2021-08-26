64eebea1bc0d8b891bf028bfbb587d452a3a1b54
mrdotx <klassiker@gmx.de>
2021-08-26 11:49:09 +0200

    minor optimizations

--- .config/xorg/X11/xinit/xinitrc.d/40-libcanberra-gtk-module.sh
+++ .config/xorg/X11/xinit/xinitrc.d/40-libcanberra-gtk-module.sh
@@ -3,16 +3,14 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/xorg/X11/xinit/xinitrc.d/40-libcanberra-gtk-module.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2021-08-25T11:11:58+0200
+# date:   2021-08-26T11:35:53+0200
 
 case "${DESKTOP_SESSION-}" in
-  gnome*) # Done by gnome-settings-daemon
-  ;;
-  *)
-    # Extra check in case DESKTOP_SESSION is not set correctly
-    if [ -z "${GNOME_DESKTOP_SESSION_ID-}" ]; then
-      GTK_MODULES="${GTK_MODULES:+$GTK_MODULES:}canberra-gtk-module"
-      export GTK_MODULES
-    fi
-  ;;
+    gnome*) # Done by gnome-settings-daemon
+        ;;
+    *)      # Extra check in case DESKTOP_SESSION is not set correctly
+        [ -z "${GNOME_DESKTOP_SESSION_ID-}" ] \
+            && GTK_MODULES="${GTK_MODULES:+$GTK_MODULES:}canberra-gtk-module" \
+            && export GTK_MODULES
+        ;;
 esac
--- .config/xorg/X11/xinit/xinitrc.d/90-gnupg-ssh-agent.sh
+++ .config/xorg/X11/xinit/xinitrc.d/90-gnupg-ssh-agent.sh
@@ -3,10 +3,9 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/xorg/X11/xinit/xinitrc.d/90-gnupg-ssh-agent.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2021-08-25T11:12:20+0200
+# date:   2021-08-26T11:36:42+0200
 
 unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-    SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-    export SSH_AUTH_SOCK
-fi
+[ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ] \
+    && SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)" \
+    && export SSH_AUTH_SOCK
