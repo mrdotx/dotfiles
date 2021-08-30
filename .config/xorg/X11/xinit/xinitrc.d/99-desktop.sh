6fa35e41e9dd860b4311aeb1cf44b52c654a0f97
mrdotx <klassiker@gmx.de>
2021-08-30 18:47:51 +0200

    default screen layout mechanism changed

--- .config/xorg/X11/xinit/xinitrc.d/99-desktop.sh
+++ .config/xorg/X11/xinit/xinitrc.d/99-desktop.sh
@@ -3,10 +3,14 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/xorg/X11/xinit/xinitrc.d/99-desktop.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2021-08-25T12:18:44+0200
+# date:   2021-08-30T18:13:35+0200
 
-command -v screenlayout.sh > /dev/null 2>&1 \
-    && screenlayout.sh &
+ xrandr | grep "HDMI2 connected" \
+    && xrandr \
+        --output "HDMI2" \
+        --primary \
+        --output "eDP1" \
+        --right-of "HDMI2"
 
 command -v wallpaper.sh > /dev/null 2>&1 \
     && wallpaper.sh &
