9a0c7471d822263bef460de280b1e5150ef3f15f
mrdotx <klassiker@gmx.de>
2021-08-30 20:37:05 +0200

    set wallpaper with systemd service

--- .config/xorg/X11/xinit/xinitrc.d/20-monitor.sh
+++ .config/xorg/X11/xinit/xinitrc.d/20-monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# path:   /home/klassiker/.local/share/repos/dotfiles/.config/xorg/X11/xinit/xinitrc.d/99-desktop.sh
+# path:   /home/klassiker/.local/share/repos/dotfiles/.config/xorg/X11/xinit/xinitrc.d/20-monitor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2021-08-30T18:13:35+0200
+# date:   2021-08-30T20:36:51+0200
 
  xrandr | grep "HDMI2 connected" \
     && xrandr \
@@ -11,6 +11,3 @@
         --primary \
         --output "eDP1" \
         --right-of "HDMI2"
-
-command -v wallpaper.sh > /dev/null 2>&1 \
-    && wallpaper.sh &
