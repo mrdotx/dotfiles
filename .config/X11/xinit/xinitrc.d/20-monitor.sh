ecb3b0cf1a9359fb890c0a9891657606cfb5154e
mrdotx <klassiker@gmx.de>
2022-03-05 17:32:17 +0100

    displayport config added

--- .config/X11/xinit/xinitrc.d/20-monitor.sh
+++ .config/X11/xinit/xinitrc.d/20-monitor.sh
@@ -3,24 +3,27 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/X11/xinit/xinitrc.d/20-monitor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2022-02-22T10:20:19+0100
+# date:   2022-03-05T16:33:07+0100
 
-xrandr | grep "DisplayPort-0 disconnected" \
+xrandr | grep "DisplayPort-0 connected" \
     && xrandr \
-        --output "HDMI-A-0" --primary \
+        --output "DisplayPort-0" --primary \
         --mode "1920x1080" \
-        --pos "0x0" \
-        --rate "74.99" \
+        --pos "0x780" \
+        --rate "144" \
         --rotate "normal" \
-        --output "DisplayPort-0" \
-        --right-of "HDMI-A-0"
+        --output "HDMI-A-0" \
+        --mode "1920x1080" \
+        --pos "1920x0" \
+        --rate "75" \
+        --rotate "left"
 
 xrandr | grep "HDMI2 connected" \
     && xrandr \
         --output "HDMI2" --primary \
         --mode "1920x1080" \
         --pos "0x0" \
-        --rate "74.99" \
+        --rate "75" \
         --rotate "normal" \
         --output "eDP1" \
         --right-of "HDMI2"
