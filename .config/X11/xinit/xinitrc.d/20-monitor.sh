28dfd2868acacce296a54add7e157a7182dc7f3b
mrdotx <klassiker@gmx.de>
2022-03-12 07:31:09 +0100

    screen rotation changed

--- .config/X11/xinit/xinitrc.d/20-monitor.sh
+++ .config/X11/xinit/xinitrc.d/20-monitor.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/X11/xinit/xinitrc.d/20-monitor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2022-03-06T17:11:34+0100
+# date:   2022-03-11T21:53:02+0100
 
 xrandr | grep "DisplayPort-0 connected" \
     && xrandr \
@@ -14,7 +14,7 @@ xrandr | grep "DisplayPort-0 connected" \
         --output "HDMI-A-0" \
         --mode "1920x1080" \
         --pos "1920x0" \
-        --rotate "left"
+        --rotate "right"
 
 xrandr | grep "HDMI2 connected" \
     && xrandr \
