10e29c954094a57fdcb2d4e6bf259d38fe2f9163
mrdotx <klassiker@gmx.de>
2023-05-27 07:38:24 +0200

    locate replaced by find

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -2,7 +2,7 @@
 path:   /home/klassiker/.local/share/repos/dotfiles/.config/ranger/commands.py
 author: klassiker [mrdotx]
 github: https://github.com/mrdotx/dotfiles
-date:   2023-05-01T17:36:38+0200
+date:   2023-05-27T07:04:53+0200
 """
 
 from __future__ import (absolute_import, division, print_function)
@@ -19,38 +19,12 @@ class FzfFind(Command):
     Search(find) for files/folders and use fzf to preview(highlight)/select.
     """
     def execute(self):
-        command = "find -L . \\( -path '*/\\.*' \
-                        -o -fstype 'dev' \
-                        -o -fstype 'proc' \\) \
-                    -prune -o -print 2> /dev/null \
+        command = "find . -name '*' 2> /dev/null \
                 | sed 1d \
                 | cut -b3- \
                 | fzf -e \
-                --preview-window 'up:75%:wrap' \
-                    --preview 'highlight {1}'"
-        fzf = self.fm.execute_command(command, stdout=PIPE)
-        stdout, sys.stderr = fzf.communicate()
-        if fzf.returncode == 0:
-            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
-            if os.path.isdir(fzf_file):
-                self.fm.cd(fzf_file)
-            else:
-                self.fm.select_file(fzf_file)
-
-# fuzzy locate files
-class FzfLocate(Command):
-    """
-    :FzfLocate
-
-    Search(locate) for files/folders and use fzf to preview(highlight)/select.
-    """
-    def execute(self):
-        command = "fzf_path=\"$(pwd)\"; \
-                plocate $fzf_path \
-                | sed \"1d;s#$fzf_path/##g\" \
-                | fzf -e \
-                --preview-window 'up:75%:wrap' \
-                    --preview 'highlight {1}'"
+                    --preview-window 'up:75%:wrap' \
+                    --preview 'highlight {}'"
         fzf = self.fm.execute_command(command, stdout=PIPE)
         stdout, sys.stderr = fzf.communicate()
         if fzf.returncode == 0:
@@ -71,8 +45,8 @@ class FzfTagged(Command):
         command = "sort \"$HOME/.local/share/ranger/tagged\" \
                 | cut -d':' -f2 \
                 | fzf -e \
-                --preview-window 'up:75%:wrap' \
-                    --preview 'highlight {1}'"
+                    --preview-window 'up:75%:wrap' \
+                    --preview 'highlight {}'"
         fzf = self.fm.execute_command(command, stdout=PIPE)
         stdout, sys.stderr = fzf.communicate()
         if fzf.returncode == 0:
@@ -97,10 +71,10 @@ class FzfGrep(Command):
             self.fm.notify("Usage: fzfgrep \"<search string>\"", bad=True)
             return
 
-        command = "grep --color=never -irsl -- " + search_string + " \
+        command = "grep --color=never -Iirsl " + search_string + " \
                 | fzf -e \
-                --preview-window 'up:75%:wrap' \
-                    --preview 'highlight {1}'"
+                    --preview-window 'up:75%:wrap' \
+                    --preview 'highlight {}'"
         fzf = self.fm.execute_command(command, stdout=PIPE)
         stdout, sys.stderr = fzf.communicate()
         if fzf.returncode == 0:
