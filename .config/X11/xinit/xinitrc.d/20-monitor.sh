6fcab94dc42b2e7d543ee5a40c9f5056fbd055bb
mrdotx <klassiker@gmx.de>
2024-01-29 17:02:34 +0100

    monitor position changed

--- .config/X11/xinit/xinitrc.d/20-monitor.sh
+++ .config/X11/xinit/xinitrc.d/20-monitor.sh
@@ -3,18 +3,17 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/X11/xinit/xinitrc.d/20-monitor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2022-07-14T11:11:00+0200
+# date:   2024-01-29T15:40:31+0100
 
 xrandr | grep "DisplayPort-0 connected" \
     && xrandr \
         --output "DisplayPort-0" --primary \
         --mode "1920x1080" \
-        --pos "0x177" \
+        --pos "0x1080" \
         --rotate "normal" \
         --output "HDMI-A-0" \
         --mode "1920x1080" \
-        --pos "1920x0" \
-        --rotate "right"
+        --pos "0x0" \
 
 xrandr | grep "HDMI2 connected" \
     && xrandr \
