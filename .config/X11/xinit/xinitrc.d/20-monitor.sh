8d02e225212143e782c3b4b31e7c42d3efab6769
mrdotx <klassiker@gmx.de>
2024-02-04 09:32:07 +0100

    xrandr options streamlined

--- .config/X11/xinit/xinitrc.d/20-monitor.sh
+++ .config/X11/xinit/xinitrc.d/20-monitor.sh
@@ -3,23 +3,14 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/X11/xinit/xinitrc.d/20-monitor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2024-01-29T15:40:31+0100
+# date:   2024-02-03T18:40:36+0100
 
-xrandr | grep "DisplayPort-0 connected" \
+xrandr | grep -q "DisplayPort-0 connected" \
     && xrandr \
         --output "DisplayPort-0" --primary \
-        --mode "1920x1080" \
-        --pos "0x1080" \
-        --rotate "normal" \
-        --output "HDMI-A-0" \
-        --mode "1920x1080" \
-        --pos "0x0" \
+        --output "HDMI-A-0" --above "DisplayPort-0"
 
-xrandr | grep "HDMI2 connected" \
+xrandr | grep -q "HDMI2 connected" \
     && xrandr \
         --output "HDMI2" --primary \
-        --mode "1920x1080" \
-        --pos "0x0" \
-        --rotate "normal" \
-        --output "eDP1" \
-        --right-of "HDMI2"
+        --output "eDP1" --right-of "HDMI2"
