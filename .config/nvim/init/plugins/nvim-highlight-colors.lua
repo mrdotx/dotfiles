6e1b3e287b7fed725eccdae3d2b7ed748d798421
mrdotx <klassiker@gmx.de>
2024-05-24 06:58:40 +0200

    new config values added

--- .config/nvim/init/plugins/nvim-highlight-colors.lua
+++ .config/nvim/init/plugins/nvim-highlight-colors.lua
@@ -1,7 +1,7 @@
 -- path:   /home/klassiker/.local/share/repos/dotfiles/.config/nvim/init/plugins/nvim-highlight-colors.lua
 -- author: klassiker [mrdotx]
 -- github: https://github.com/mrdotx/dotfiles
--- date:   2024-05-20T17:12:17+0200
+-- date:   2024-05-23T20:26:43+0200
 
 require("nvim-highlight-colors").setup {
     -- Render style
@@ -10,6 +10,31 @@ require("nvim-highlight-colors").setup {
 
     -- Set virtual symbol (requires render to be set to 'virtual')
     virtual_symbol = '■',
+
+    -- Set virtual symbol suffix (defaults to '')
+    virtual_symbol_prefix = '',
+
+    -- Set virtual symbol suffix (defaults to ' ')
+    virtual_symbol_suffix = ' ',
+
+    -- Set virtual symbol position()
+    -- @usage 'inline'|'eol'|'eow'
+    -- inline mimics VS Code style
+    -- eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+    -- eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+    virtual_symbol_position = 'inline',
+
+    -- Highlight hex colors, e.g. '#FFFFFF'
+    enable_hex = true,
+
+    -- Highlight rgb colors, e.g. 'rgb(0 0 0)'
+    enable_rgb = true,
+
+    -- Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+    enable_hsl = true,
+
+    -- Highlight CSS variables, e.g. 'var(--testing-color)'
+    enable_var_usage = true,
 
     -- Highlight named colors, e.g. 'green'
     enable_named_colors = true,
