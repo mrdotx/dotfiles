e2275924062957b7f21dbc2902d3c2bd020a945d
mrdotx <klassiker@gmx.de>
2025-06-25 05:41:34 +0200

    mechanism changed

--- .config/X11/xinit/xinitrc.d/20-monitor.sh
+++ .config/X11/xinit/xinitrc.d/20-monitor.sh
@@ -3,14 +3,25 @@
 # path:   /home/klassiker/.local/share/repos/dotfiles/.config/X11/xinit/xinitrc.d/20-monitor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/dotfiles
-# date:   2024-02-03T18:40:36+0100
+# date:   2025-06-25T05:41:29+0200
 
-xrandr | grep -q "DisplayPort-0 connected" \
-    && xrandr \
-        --output "DisplayPort-0" --primary \
-        --output "HDMI-A-0" --above "DisplayPort-0"
+output_state=$(xrandr)
 
-xrandr | grep -q "HDMI2 connected" \
-    && xrandr \
-        --output "HDMI2" --primary \
-        --output "eDP1" --right-of "HDMI2"
+connected() {
+    printf "%s" "$output_state" | grep -q "$1 connected"
+}
+
+# secondary monitor
+if connected "HDMI-A-0"; then
+    # desktop primary monitor
+    connected "DisplayPort-0" \
+        && xrandr \
+            --output "DisplayPort-0" --primary \
+            --output "HDMI-A-0" --above "DisplayPort-0"
+
+    # notebook primary monitor
+    connected "eDP" \
+        && xrandr \
+            --output "HDMI-A-0" --primary \
+            --output "eDP" --right-of "HDMI-A-0"
+fi
